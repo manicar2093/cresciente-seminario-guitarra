3ff05a12eb53e05235740b93824f8c968fe1c11c
Manuel Carbajal <manicar2093@gmail.com>
2026-02-03 20:18:44 -0600

    feat: add button improvements

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -146,19 +146,47 @@
 			>
 			Aleatorio
 		</button>
-		<div class="flex flex-row space-x-1 mt-6">
+		<div class="flex flex-row space-x-1 mt-3">
 			<button
 				onclick={selectAll}
 				disabled={todos_seleccionados}
-				class="w-full bg-emerald-600 hover:bg-emerald-700 disabled:bg-gray-400 disabled:cursor-not-allowed text-white font-bold py-2 px-4 rounded-lg transition-colors shadow-md flex items-center justify-center gap-2"
+				class="w-full bg-emerald-600 hover:bg-emerald-700 disabled:bg-gray-400 disabled:cursor-not-allowed text-white font-bold py-2 px-4 rounded-lg transition-colors shadow-md flex items-center justify-center gap-2 text-sm"
 			>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="20"
+					height="20"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					class="lucide lucide-check-square"
+					><polyline points="9 11 12 14 22 4" /><path
+						d="M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11"
+					/></svg
+				>
 				Seleccionar Todos
 			</button>
 			<button
 				onclick={deselectAll}
 				disabled={!todos_seleccionados}
-				class="w-full bg-emerald-600 hover:bg-emerald-700 disabled:bg-gray-400 disabled:cursor-not-allowed text-white font-bold py-2 px-4 rounded-lg transition-colors shadow-md flex items-center justify-center gap-2"
+				class="w-full bg-emerald-600 hover:bg-emerald-700 disabled:bg-gray-400 disabled:cursor-not-allowed text-white font-bold py-2 px-4 rounded-lg transition-colors shadow-md flex items-center justify-center gap-2 text-sm"
 			>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="20"
+					height="20"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					class="lucide lucide-square"
+					><rect width="18" height="18" x="3" y="3" rx="2" /></svg
+				>
 				Deseleccionar Todos
 			</button>
 
